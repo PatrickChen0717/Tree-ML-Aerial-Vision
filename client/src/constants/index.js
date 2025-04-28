45a5f2a1de080cc639e7b92e22e50f2abb815f13
PatrickChen0717 <patric18@student.ubc.ca>
2025-04-28 05:17:22 -0400

    Bug fix: config url

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,8 +1,8 @@
 import { phone_icon_green, drone_icon_green, coords, history_icon, satellite_icon_green, grid_icon, lidar_icon, tree_icon } from "../assets/icons";
 import { good_example, bad_example_busy, bad_example_size, toggle_partial, toggle_genus, resolution_estimation, long_lat, good_image_drone_rgb, bad_image_drone_rgb } from "../assets/images";
 
-export const backendURL = import.meta.env.SERVER_IP;
-
+export const backendURL = import.meta.env.VITE_SERVER_IP;
+console.log('backendURL:', backendURL);
 export const helpLinks = [
   {
     label: 'Phone Classification',
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Manually load env from one level up
+  const env = loadEnv(mode, '../')
+
+  return {
+    plugins: [react()],
+    define: {
+      'import.meta.env.VITE_SERVER_IP': JSON.stringify(env.VITE_SERVER_IP),
+    },
+  }
 })
